9e91990d2708c1eecc73ce5c0adfac4771e5c4bc
eminalpernl <eminalpernl@gmail.com>
2023-01-24 23:15:55 +0100

    server.js seperated successfully

---
+++ homework/hackyourtemperature/app.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import path from 'path';
+import bodyParser from "body-parser";
+import { dirname } from 'path';
+import { fileURLToPath } from "url";
+import fetch from 'node-fetch';
+import { keys } from "./sources/keys.js";
+
+
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const app = express();
+const port = 3000;
+
+
+
+
+export const grabHtml = (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+};
+
+
+export const getWeatherForecast = async (req, res) => {
+  const cityName = req.body.cityName;
+  console.log(cityName);
+  const api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${keys.API_KEY}&units=metric`;
+  console.log(api);
+  const response = await fetch(api, {method: 'POST', body: 'a=1'});
+  console.log(response);
+  try {
+    if (response.status === 200) {
+      const WeatherForecast = await response.json();
+      const currentTemp = WeatherForecast.main.temp;
+      const feelsLike = WeatherForecast.main.feels_like;
+
+      res.send({
+        Message: `It is ${currentTemp} degree in ${cityName}. It feels like ${feelsLike} `,
+      });
+    } else{
+      res.send({ Message: `There is no data with the name of ${cityName}` })
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  
+
+};
+
+
+export const server = (req, res) => {
+
+  console.log(`app listening on port ${port}`);
+}
+
--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,12 +1,8 @@
 import express from 'express';
 import path from 'path';
-import bodyParser from "body-parser";
 import { dirname } from 'path';
 import { fileURLToPath } from "url";
-import fetch from 'node-fetch';
-import { keys } from "./sources/keys.js";
-
-
+import { grabHtml, getWeatherForecast, server } from './app.js';
 
 
 const __filename = fileURLToPath(import.meta.url)
@@ -18,49 +14,16 @@ const port = 3000;
 
 // GET method route---------------------
 
-app.get('/', (req, res) => {
-  //res.end('naper')
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.get('/', grabHtml);
 
+// init middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //POST method route----------------------
 
-app.post('/weather', async (req, res) => {
-  const cityName = req.body.cityName;
-  console.log(cityName);
-  const api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${keys.API_KEY}&units=metric`;
-  console.log(api);
-  const response = await fetch(api, {method: 'POST', body: 'a=1'});
-  console.log(response);
-  try {
-    if (response.status === 200) {
-      const WeatherForecast = await response.json();
-      const currentTemp = WeatherForecast.main.temp;
-      const feelsLike = WeatherForecast.main.feels_like;
+app.post('/weather', getWeatherForecast);
 
-      res.send({
-        Message: `It is ${currentTemp} degree in ${cityName}. It feels like ${feelsLike} `,
-      });
-    } else{
-      res.send({ Message: `There is no data with the name of ${cityName}` })
-    }
-  } catch (error) {
-    console.error(error);
-  }
-  
-
-});
-
-
-
-
-
-app.listen(port, (req, res) => {
-
-  console.log(`app listening on port ${port}`);
-});
+app.listen(port, server);
 
 
